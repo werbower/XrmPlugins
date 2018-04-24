57d57da588e32f450e7ca6f0b7de0c32ca56f1de
werbower <werbower@gmail.com>
2018-04-24 13:03:29 +0300

    create message

--- XrmPlugins/MyPlugin.cs
+++ XrmPlugins/MyPlugin.cs
@@ -6,8 +6,7 @@ using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace XrmPlugins
-{
+namespace XrmPlugins {
     public class MyPlugin : IPlugin {
         public void Execute(IServiceProvider serviceProvider) {
             // Extract the tracing service for use in debugging sandboxed plug-ins.  
@@ -19,90 +18,118 @@ namespace XrmPlugins
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
             // The InputParameters collection contains all the data passed in the message request.  
-            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity) {
-                // Obtain the target entity from the input parameters.  
-                Entity entity = (Entity)context.InputParameters["Target"];
+            //if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity) {
+            // Obtain the target entity from the input parameters.  
 
-                // Verify that the target entity represents an entity type you are expecting.   
-                // For example, an account. If not, the plug-in was not registered correctly.  
-                if (entity.LogicalName != "account")
-                    return;
 
-                {
-                    Entity followupTask = new Entity("task");
-                    followupTask["subject"] = "send e-mail to the new customer";
-                    followupTask["description"] = "follow up with the customer";
-                    followupTask["scheduledstart"] = DateTime.Now.AddDays(7);
-                    followupTask["scheduledend"] = DateTime.Now.AddDays(7);
-                    followupTask["category"] = context.PrimaryEntityName;
-                    if (context.OutputParameters.Contains("id")) {
-                        Guid regardingId = new Guid(context.OutputParameters["id"].ToString());
-                        string regardingType = "account";
-                        followupTask["regardingobjectid"] = new EntityReference(regardingType, regardingId);
-                    }
-                    // Obtain the organization service reference which you will need for  
-                    // web service calls.  
-                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
-                    
+            // Verify that the target entity represents an entity type you are expecting.   
+            // For example, an account. If not, the plug-in was not registered correctly.  
+            Entity entity = (Entity)context.InputParameters["Target"];
+            if (entity.LogicalName == "new_specialentity")// audit entity
+                return;
+            #region sample
 
-                    try {
-                        // Plug-in business logic goes here.  
-                        service.Create(followupTask);
-                    } catch (FaultException<OrganizationServiceFault> ex) {
-                        throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
-                    } catch (Exception ex) {
-                        tracingService.Trace("MyPlugin: {0}", ex.ToString());
-                        throw;
-                    }
+            //it was in sample
+            //{
+            //    Entity followupTask = new Entity("task");
+            //    followupTask["subject"] = "send e-mail to the new customer";
+            //    followupTask["description"] = "follow up with the customer";
+            //    followupTask["scheduledstart"] = DateTime.Now.AddDays(7);
+            //    followupTask["scheduledend"] = DateTime.Now.AddDays(7);
+            //    followupTask["category"] = context.PrimaryEntityName;
+            //    if (context.OutputParameters.Contains("id")) {
+            //        Guid regardingId = new Guid(context.OutputParameters["id"].ToString());
+            //        string regardingType = "account";
+            //        followupTask["regardingobjectid"] = new EntityReference(regardingType, regardingId);
+            //    }
+            //    // Obtain the organization service reference which you will need for  
+            //    // web service calls.  
+            //    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            //    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
-                }
-                {
-                    if (!context.OutputParameters.Contains("id"))
-                        return;
 
-                    // Obtain the organization service reference which you will need for  
-                    // web service calls.  
-                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            //    try {
+            //        // Plug-in business logic goes here.  
+            //        service.Create(followupTask);
+            //    } catch (FaultException<OrganizationServiceFault> ex) {
+            //        throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
+            //    } catch (Exception ex) {
+            //        tracingService.Trace("MyPlugin: {0}", ex.ToString());
+            //        throw;
+            //    }
 
-                    Entity auditEntity = new Entity("new_specialentity");
-                    auditEntity["new_name"] = entity.LogicalName;
-                    auditEntity["new_spaction"] = "create me";
-                    //author
-                    Guid authorId = new Guid(context.InitiatingUserId.ToString());
-                    string authorType = "systemuser";
-                    auditEntity["new_spauthor"] = new EntityReference(authorType, authorId);
-                    //name
-                    Entity details = service.Retrieve(entity.LogicalName, entity.Id,
-                        new Microsoft.Xrm.Sdk.Query.ColumnSet("name"));
-                    string accountName = details.Attributes.Contains("name") ? ((String)details["name"]) : "empty name";
-                    auditEntity["new_spentity"] = accountName;
-                    //
-                    try {
-                        // Plug-in business logic goes here.  
-                        service.Create(auditEntity);
-                    } catch (FaultException<OrganizationServiceFault> ex) {
-                        throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
-                    } catch (Exception ex) {
-                        tracingService.Trace("MyPlugin: {0}", ex.ToString());
-                        throw;
-                    }
+            //} 
+            #endregion
 
+
+
+            //create operation
+            #region Create operation
+            if (context.PostEntityImages.Contains("create")) {
+                //if not created
+                if (!context.OutputParameters.Contains("id")) {
+                    throw new InvalidPluginExecutionException("can't see id in output");
                 }
 
+                //Entity preEntity = (Entity)context.PreEntityImages["create"];
+                Entity postEntity = (Entity)context.PostEntityImages["create"];
 
 
+                // Obtain the organization service reference which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
+
+                Entity auditEntity = new Entity("new_specialentity");
+                auditEntity["new_name"] = entity.LogicalName;
+                auditEntity["new_spaction"] = "created";
+                //author
+                Guid authorId = new Guid(context.InitiatingUserId.ToString());
+                string authorType = "systemuser";
+                auditEntity["new_spauthor"] = new EntityReference(authorType, authorId);
+                //name
+                //Entity details = service.Retrieve(entity.LogicalName, entity.Id,new Microsoft.Xrm.Sdk.Query.ColumnSet("name"));
+                //string accountName = details.Attributes.Contains("name") ? ((String)details["name"]) : "empty name";
+                string name = "empty name";
+                if (postEntity.Contains("name"))
+                    name = postEntity.GetAttributeValue<string>("name");
+
+                auditEntity["new_spentity"] = name;
+                //
                 try {
                     // Plug-in business logic goes here.  
+                    service.Create(auditEntity);
                 } catch (FaultException<OrganizationServiceFault> ex) {
-                    throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
+                    throw new InvalidPluginExecutionException("An error occurred in MyPlugin.", ex);
                 } catch (Exception ex) {
                     tracingService.Trace("MyPlugin: {0}", ex.ToString());
                     throw;
                 }
+            } else {
+                throw new InvalidPluginExecutionException("can't see postimage");
             }
+            #endregion
+
+
+
+
+
+
+            //}
+
+
+
+
+            try {
+                // Plug-in business logic goes here.  
+            } catch (FaultException<OrganizationServiceFault> ex) {
+                throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
+            } catch (Exception ex) {
+                tracingService.Trace("MyPlugin: {0}", ex.ToString());
+                throw;
+            }
+
         }
     }
 }
